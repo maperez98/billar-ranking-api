db1996a82986977a85e99b54da28e301d9033b15
miguel-angel-perez-vargas <miguelangelperezvargas04@gmail.com>
2026-04-26 00:39:47 -0500

    Prueba con github - Push correcto

--- models.py
+++ models.py
@@ -5,24 +5,25 @@ from nivel_Jugador import NivelJugador
 
 
 class Jugador(BaseModel):
-    id: int = Field(..., description="Identificador único del jugador")
+    id: int = Field(..., description="Id del jugador")
     nombre: str = Field(..., min_length=2, max_length=50)
     edad: int = Field(..., ge=10, le=100)
     pais: str = Field(..., min_length=2, max_length=50)
     nivel: NivelJugador
 
 
-# 🎱 MODELO: Partida
 class Partida(BaseModel):
-    id: int = Field(..., description="Identificador único de la partida")
+    id: int = Field(..., description="Id de la partida")
     jugador1_id: int = Field(..., description="ID del primer jugador")
     jugador2_id: int = Field(..., description="ID del segundo jugador")
     ganador_id: int = Field(..., description="ID del jugador ganador")
     fecha: date = Field(..., description="Fecha de la partida")
 
 
-# 🏆 MODELO: Ranking
+
 class Ranking(BaseModel):
     jugador_id: int = Field(..., description="ID del jugador")
     puntos: int = Field(0, ge=0)
     posicion: int = Field(..., ge=1)
+
+    #Prueba2 github
