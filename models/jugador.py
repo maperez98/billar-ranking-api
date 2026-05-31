5bb9ab8f56794d556c0674c207cfadae2bd4b21a
miguel-angel-perez-vargas <miguelangelperezvargas04@gmail.com>
2026-05-31 01:11:23 -0500

    Nuevos cambios para render

--- models/jugador.py
+++ models/jugador.py
@@ -1,12 +1,14 @@
 from sqlmodel import SQLModel, Field
+from models.nivel_jugador import NivelJugador
 from typing import Optional
 
+
 class JugadorBase(SQLModel):
-    nombre: str
-    edad: int
-    pais: str
-    nivel: str
-    activo: bool = True
+    nombre: str = Field(..., min_length=2, max_length=50)
+    edad: int = Field(..., ge=18, le=100)
+    pais: str = Field(..., min_length=2, max_length=50)
+    nivel: NivelJugador = Field(...)
+    activo: bool = Field(default=True)
 
 
 class JugadorID(JugadorBase, table=True):
@@ -15,8 +17,8 @@ class JugadorID(JugadorBase, table=True):
 
 
 class JugadorUpdate(SQLModel):
-    nombre: Optional[str] = None
-    edad: Optional[int] = None
-    pais: Optional[str] = None
-    nivel: Optional[str] = None
-    activo: Optional[bool] = None
+    nombre: Optional[str] = Field(default=None, min_length=2, max_length=50)
+    edad: Optional[int] = Field(default=None, ge=18, le=100)
+    pais: Optional[str] = Field(default=None, min_length=2, max_length=50)
+    nivel: Optional[NivelJugador] = Field(default=None)
+    activo: Optional[bool] = Field(default=None)
