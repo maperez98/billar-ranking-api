66ac5e35fe0c5bbbf9a832677d2fbdd5d8974989
miguel-angel-perez-vargas <miguelangelperezvargas04@gmail.com>
2026-05-31 01:05:10 -0500

    Prueba para models/jugador.py

--- models/jugador.py
+++ models/jugador.py
@@ -1,14 +1,12 @@
 from sqlmodel import SQLModel, Field
-from models.nivel_jugador import NivelJugador
 from typing import Optional
 
-
 class JugadorBase(SQLModel):
-    nombre: str = Field(..., min_length=2, max_length=50)
-    edad: int = Field(..., ge=18, le=100)
-    pais: str = Field(..., min_length=2, max_length=50)
-    nivel: NivelJugador = Field(...)
-    activo: bool = Field(default=True)
+    nombre: str
+    edad: int
+    pais: str
+    nivel: str
+    activo: bool = True
 
 
 class JugadorID(JugadorBase, table=True):
@@ -17,8 +15,8 @@ class JugadorID(JugadorBase, table=True):
 
 
 class JugadorUpdate(SQLModel):
-    nombre: Optional[str] = Field(default=None, min_length=2, max_length=50)
-    edad: Optional[int] = Field(default=None, ge=18, le=100)
-    pais: Optional[str] = Field(default=None, min_length=2, max_length=50)
-    nivel: Optional[NivelJugador] = Field(default=None)
-    activo: Optional[bool] = Field(default=None)
+    nombre: Optional[str] = None
+    edad: Optional[int] = None
+    pais: Optional[str] = None
+    nivel: Optional[str] = None
+    activo: Optional[bool] = None
