2432a8c993d0f2919a1d57b4f4c21f1342dca457
miguel-angel-perez-vargas <miguelangelperezvargas04@gmail.com>
2026-05-30 21:26:16 -0500

    X save

--- operations_csv.py
+++ operations_csv.py
@@ -14,7 +14,7 @@ def crear_jugador(jugador: Jugador):
             archivo_existe = True
     except FileNotFoundError:
         pass
-
+# hol
     with open(FILE_JUGADORES, mode="a", newline="") as file:
         writer = csv.writer(file)
 
