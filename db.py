076033455661fbbe42198df9798a982bb59cfb24
miguel-angel-perez-vargas <miguelangelperezvargas04@gmail.com>
2026-05-31 02:01:54 -0500

    Cambios en db.py

--- db.py
+++ db.py
@@ -3,23 +3,28 @@ from dotenv import load_dotenv
 from sqlmodel import Session, create_engine, SQLModel
 from fastapi import FastAPI, Depends
 from typing import Annotated
+from contextlib import asynccontextmanager
 
 load_dotenv()
 
 neon_db = os.getenv("DATABASE_URL_NEON")
 
-engine = create_engine(neon_db)
 
+engine = create_engine(neon_db, echo=True)
 
-def create_all_tables(app: FastAPI):
-    if os.getenv("ENV") == "dev":
-        SQLModel.metadata.create_all(engine)
+@asynccontextmanager
+async def create_all_tables(app: FastAPI):
+    try:
+        if os.getenv("ENV") == "dev":
+            print("⏳ Intentando conectar y crear tablas en Neon...")
+            SQLModel.metadata.create_all(engine)
+            print(" Tablas sincronizadas con éxito")
+    except Exception as e:
+        print(f" Error fatal de base de datos: {e}")
     yield
-
 
 def get_session() -> Session:
     with Session(engine) as session:
         yield session
-
 
 SessionDep = Annotated[Session, Depends(get_session)]
