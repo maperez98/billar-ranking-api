d794c046df701a13a838b558e325b9f4dcfce569
miguel-angel-perez-vargas <miguelangelperezvargas04@gmail.com>
2026-05-31 22:43:47 -0500

    Conexion neon a render

--- db.py
+++ db.py
@@ -10,14 +10,22 @@ load_dotenv()
 neon_db = os.getenv("DATABASE_URL_NEON")
 
 
-engine = create_engine(neon_db, echo=True)
+engine = create_engine(
+    neon_db,
+    echo=True,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=10
+)
 
 @asynccontextmanager
 async def create_all_tables(app: FastAPI):
-    print("⏳ Intentando conectar y asegurar tablas en Neon...")
+    print("Intentando conectar y asegurar tablas en Neon...")
     SQLModel.metadata.create_all(engine)
     print("Tablas listas y sincronizadas")
     yield
+
 def get_session() -> Session:
     with Session(engine) as session:
         yield session
